d989e8065317b1fab731a8f57edd03782575cabe
aboubakrderamchi <aboubakrderamchi@gmail.com>
2023-12-03 10:59:15 -0500

    second commit

--- src/test/java/ptProg/GitTest.java
+++ src/test/java/ptProg/GitTest.java
@@ -2,10 +2,15 @@ package ptProg;
 
 public class GitTest {
 	public static void main(String[] args) {
-		printMe();
+		printMe1();
+		printMe2();
 	}
 
-	public static void printMe() {
+	public static void printMe1() {
 		System.out.println("Git Test");
 	}
+	public static void printMe2() {
+		System.out.println("Git Test");
+	}
+	
 }
